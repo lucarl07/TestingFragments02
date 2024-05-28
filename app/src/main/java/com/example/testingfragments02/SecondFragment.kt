8978fd879d6fe9e9be2d7b47efb58dc5729fc011
lucarl07 <junitinho2007@gmail.com>
2024-05-27 22:06:58 -0300

    Create the second fragment + update the 1st one

--- app/src/main/java/com/example/testingfragments02/SecondFragment.kt
+++ app/src/main/java/com/example/testingfragments02/SecondFragment.kt
@@ -2,6 +2,6 @@ package com.example.testingfragments02
 
 import androidx.fragment.app.Fragment
 
-class FirstFragment : Fragment(R.layout.fragment_first) {
+class SecondFragment : Fragment(R.layout.fragment_second) {
 
 }
