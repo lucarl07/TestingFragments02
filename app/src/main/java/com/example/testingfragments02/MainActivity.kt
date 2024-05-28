66e12db50e5f4c2f38751578a23765e2ed92c40a
lucarl07 <junitinho2007@gmail.com>
2024-05-27 23:11:31 -0300

    Finish the "Swap Fragment" functionality

--- app/src/main/java/com/example/testingfragments02/MainActivity.kt
+++ app/src/main/java/com/example/testingfragments02/MainActivity.kt
@@ -3,18 +3,37 @@ package com.example.testingfragments02
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import com.example.testingfragments02.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        super.onCreate(savedInstanceState); enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val firstFragment = FirstFragment()
+        val secondFragment = SecondFragment()
+
+        // Initialize the frame view with an already selected fragment:
+        swapFragments(R.id.fl_fragment, firstFragment)
+
+        binding.btnFragment01.setOnClickListener {
+            swapFragments(R.id.fl_fragment, firstFragment)
+        }
+        binding.btnFragment02.setOnClickListener {
+            swapFragments(R.id.fl_fragment, secondFragment)
+        }
+    }
+
+    private fun swapFragments(frameView: Int, fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(frameView, fragment)
+            addToBackStack(null)
+            commit()
         }
     }
 }
